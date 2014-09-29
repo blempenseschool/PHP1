198ec363f39727c6cdb5f6da0afd31d359677f3f
Bert <blempens@eschool.be>
2014-09-29 11:50:55 +0200

    Deleted opdrachten and dagboek folders/files

---
+++ 06 - MySQL/Oefeningen/MySQL/01 - Oefening mysql.sql
@@ -0,0 +1,29 @@
+-- Selecteer alle leraren met als naam "Toon"
+
+
+
+-- Wijzig de achternaam van de leraar met als ID 5 naar 'Drijvers'
+
+
+
+-- Selecteer alle leraren die 'nys' in hun achternaam bevatten. Dus niet alleen nys maar ook bv. "nyssen"
+
+
+
+-- Verwijder alle leraren die als geboorte jaar '1985' hebben
+
+
+
+-- Selecteer alle leraren in alfabetische volgorde (op achternaam)
+
+
+
+-- Kies de laatste drie gebruikers die toegevoegd zijn aan de tabel
+
+
+
+-- Delete de gebruikers met als ID 3, 6, 8
+
+
+
+-- Delete ook alle gebruikers met als voornaam 'Koen'
--- 07 - Project Dagboek/Oefeningen/login-systeem/09-logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-// Maak een logout pagina
-// 
-// --> verwijder de $_SESSION variabele die je gebruikt hebt
-// --> stuur terug door naar de login pagina
